3bed787ec34b5e791e394aa6313141af2b177d5b
mattak <mattak.me@gmail.com>
2015-11-22 23:35:03 +0900

    Update GeoHex.java to handle v3.2 api

--- src/main/java/org/geohex/geohex4j/GeoHex.java
+++ src/main/java/org/geohex/geohex4j/GeoHex.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
  * GeoHex V3 for Java implemented by @chshii is licensed under Creative Commons BY-SA 2.1 Japan License.
  */
 public class GeoHex {
-    public static final String VERSION = "3.00";
+    public static final String VERSION = "3.20";
 
     // *** Share with all instances ***
     public static final String h_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -18,19 +18,17 @@ public class GeoHex {
     public static final double h_k = Math.tan(h_deg);
 
     // *** Share with all instances ***
-    // private static
     private static double calcHexSize(int level) {
-        return h_base / Math.pow(3.0, level + 1);
+        return h_base / Math.pow(3.0, level + 3);
     }
 
-    // private class
     public static final class Zone {
-        public double lat;
-        public double lon;
-        public long x; // ?
-        public long y; // ?
-        public String code;
-        public int level;
+        public final double lat;
+        public final double lon;
+        public final long x;
+        public final long y;
+        public final String code;
+        public final int level;
 
         public Zone(double lat, double lon, long x, long y, String code) {
             this.lat = lat;
@@ -38,15 +36,15 @@ public class GeoHex {
             this.x = x;
             this.y = y;
             this.code = code;
-            this.level = getLevel();
+            this.level = this.getLevel();
         }
 
-        private int getLevel() {
+        public int getLevel() {
             return this.code.length() - 2;
         }
 
         public double getHexSize() {
-            return calcHexSize(this.getLevel() + 2);
+            return calcHexSize(this.getLevel());
         }
 
         public Loc[] getHexCoords() {
@@ -75,7 +73,6 @@ public class GeoHex {
         }
     }
 
-    // public static
     public static final Zone getZoneByLocation(double lat, double lon, int level) {
         if (lat < -90 || lat > 90)
             throw new IllegalArgumentException("latitude must be between -90 and 90");
@@ -84,9 +81,19 @@ public class GeoHex {
         if (level < 0 || level > 15)
             throw new IllegalArgumentException("level must be between 0 and 15");
 
-        level += 2;
-        double h_size = calcHexSize(level);
+        XY xy = getXYByLocation(lat, lon, level);
+        return getZoneByXY(xy.x, xy.y, level);
+    }
 
+    public static final Zone getZoneByCode(String code) {
+        XY xy = getXYByCode(code);
+        int level = code.length() - 2;
+        Zone zone = getZoneByXY(xy.x, xy.y, level);
+        return zone;
+    }
+
+    public static final XY getXYByLocation(double lat, double lon, int level) {
+        double h_size = calcHexSize(level);
         XY z_xy = loc2xy(lon, lat);
         double lon_grid = z_xy.x;
         double lat_grid = z_xy.y;
@@ -113,75 +120,16 @@ public class GeoHex {
             }
         }
 
-        double h_lat = (h_k * h_x * unit_x + h_y * unit_y) / 2;
-        double h_lon = (h_lat - h_y * unit_y) / h_k;
-
-        Loc z_loc = xy2loc(h_lon, h_lat);
-        double z_loc_x = z_loc.lon;
-        double z_loc_y = z_loc.lat;
-        if (h_base - h_lon < h_size) {
-            z_loc_x = 180;
-            long h_xy = h_x;
-            h_x = h_y;
-            h_y = h_xy;
-        }
-
-        StringBuffer h_code = new StringBuffer();
-        List<Integer> code3_x = new ArrayList<Integer>();
-        List<Integer> code3_y = new ArrayList<Integer>();
-        StringBuffer code3 = new StringBuffer();
-        StringBuffer code9 = new StringBuffer();
-        long mod_x = h_x;
-        long mod_y = h_y;
-
-
-        for (int i = 0; i <= level; i++) {
-            double h_pow = Math.pow(3, level - i);
-            if (mod_x >= Math.ceil(h_pow / 2)) {
-                code3_x.add(2);
-                mod_x -= h_pow;
-            } else if (mod_x <= -Math.ceil(h_pow / 2)) {
-                code3_x.add(0);
-                mod_x += h_pow;
-            } else {
-                code3_x.add(1);
-            }
-            if (mod_y >= Math.ceil(h_pow / 2)) {
-                code3_y.add(2);
-                mod_y -= h_pow;
-            } else if (mod_y <= -Math.ceil(h_pow / 2)) {
-                code3_y.add(0);
-                mod_y += h_pow;
-            } else {
-                code3_y.add(1);
-            }
-        }
-
-        for (int i = 0; i < code3_x.size(); i++) {
-            code3.append("" + code3_x.get(i) + code3_y.get(i));
-            code9.append(Integer.parseInt(code3.toString(), 3));
-            h_code.append(code9);
-            code9.setLength(0);
-            code3.setLength(0);
-        }
-        String h_2 = h_code.substring(3);
-        int h_1 = Integer.parseInt(h_code.substring(0, 3));
-        int h_a1 = (int) Math.floor(h_1 / 30);
-        int h_a2 = h_1 % 30;
-
-        StringBuffer h_code_r = new StringBuffer();
-        h_code_r.append(h_key.charAt(h_a1)).append(h_key.charAt(h_a2)).append(h_2.toString());
-        return new Zone(z_loc_y, z_loc_x, h_x, h_y, h_code_r.toString());
+        XY inner_xy = adjustXY(h_x, h_y, level);
+        return inner_xy;
     }
 
-    public static final Zone getZoneByCode(String code) {
-        int level = code.length();
-        double h_size = calcHexSize(level);
-        double unit_x = 6 * h_size;
-        double unit_y = 6 * h_size * h_k;
-        long h_x = 0;
-        long h_y = 0;
-        String h_dec9 = "" + (h_key.indexOf(code.charAt(0)) * 30 + h_key.indexOf(code.charAt(1))) + code.substring(2);
+    private static XY getXYByCode(String code) {
+        int level = code.length() - 2;
+        double h_x = 0;
+        double h_y = 0;
+
+        String h_dec9 = new StringBuffer("").append(h_key.indexOf(code.charAt(0)) * 30 + h_key.indexOf(code.charAt(1))).append(code.substring(2)).toString();
         if (regMatch(h_dec9.charAt(0), INC15) && regMatch(h_dec9.charAt(1), EXC125) && regMatch(h_dec9.charAt(2), EXC125)) {
             if (h_dec9.charAt(0) == '5') {
                 h_dec9 = "7" + h_dec9.substring(1, h_dec9.length());
@@ -189,11 +137,13 @@ public class GeoHex {
                 h_dec9 = "3" + h_dec9.substring(1, h_dec9.length());
             }
         }
+
         int d9xlen = h_dec9.length();
-        for (int i = 0; i < level + 1 - d9xlen; i++) {
+        for (int i = 0; i < level + 3 - d9xlen; i++) {
             h_dec9 = "0" + h_dec9;
             d9xlen++;
         }
+
         StringBuffer h_dec3 = new StringBuffer();
         for (int i = 0; i < d9xlen; i++) {
             int dec9i = Integer.parseInt("" + h_dec9.charAt(i));
@@ -212,8 +162,8 @@ public class GeoHex {
             h_decy.add(h_dec3.charAt(i * 2 + 1));
         }
 
-        for (int i = 0; i <= level; i++) {
-            double h_pow = Math.pow(3, level - i);
+        for (int i = 0; i <= level + 2; i++) {
+            double h_pow = Math.pow(3, level + 2 - i);
             if (h_decx.get(i) == '0') {
                 h_x -= h_pow;
             } else if (h_decx.get(i) == '2') {
@@ -226,16 +176,126 @@ public class GeoHex {
             }
         }
 
-        double h_lat_y = (h_k * h_x * unit_x + h_y * unit_y) / 2;
-        double h_lon_x = (h_lat_y - h_y * unit_y) / h_k;
+        XY inner_xy = adjustXY(h_x, h_y, level);
+        return inner_xy;
+    }
 
-        Loc h_loc = xy2loc(h_lon_x, h_lat_y);
-        if (h_loc.lon > 180) {
-            h_loc.lon -= 360;
-        } else if (h_loc.lon < -180) {
-            h_loc.lon += 360;
+    public static final Zone getZoneByXY(double x, double y, int level) {
+        double h_size = calcHexSize(level);
+        long h_x = (long) x;
+        long h_y = (long) y;
+        double unit_x = 6 * h_size;
+        double unit_y = 6 * h_size * h_k;
+        double h_lat = (h_k * h_x * unit_x + h_y * unit_y) / 2;
+        double h_lon = (h_lat - h_y * unit_y) / h_k;
+        Loc z_loc = xy2loc(h_lon, h_lat);
+        double z_loc_x = z_loc.lon;
+        double z_loc_y = z_loc.lat;
+        double max_hsteps = Math.pow(3, level + 2);
+        double hsteps = Math.abs(h_x - h_y);
+
+        if (hsteps == max_hsteps) {
+            if (h_x > h_y) {
+                long tmp = h_x;
+                h_x = h_y;
+                h_y = tmp;
+            }
+            z_loc_x = -180;
         }
-        return new Zone(h_loc.lat, h_loc.lon, h_x, h_y, code);
+
+        StringBuffer h_code = new StringBuffer();
+        List<Integer> code3_x = new ArrayList<Integer>();
+        List<Integer> code3_y = new ArrayList<Integer>();
+        StringBuffer code3 = new StringBuffer();
+        StringBuffer code9 = new StringBuffer();
+        long mod_x = (long) h_x;
+        long mod_y = (long) h_y;
+
+        for (int i = 0; i <= level + 2; i++) {
+            double h_pow = Math.pow(3, level + 2 - i);
+            if (mod_x >= Math.ceil(h_pow / 2)) {
+                code3_x.add(2);
+                mod_x -= h_pow;
+            } else if (mod_x <= -Math.ceil(h_pow / 2)) {
+                code3_x.add(0);
+                mod_x += h_pow;
+            } else {
+                code3_x.add(1);
+            }
+
+            if (mod_y >= Math.ceil(h_pow / 2)) {
+                code3_y.add(2);
+                mod_y -= h_pow;
+            } else if (mod_y <= -Math.ceil(h_pow / 2)) {
+                code3_y.add(0);
+                mod_y += h_pow;
+            } else {
+                code3_y.add(1);
+            }
+
+            if (i == 2 && (z_loc_x == -180 || z_loc_x >= 0)) {
+                if (code3_x.get(0) == 2 && code3_y.get(0) == 1 && code3_x.get(1) == code3_y.get(1) && code3_x.get(2) == code3_y.get(2)) {
+                    code3_x.set(0, 1);
+                    code3_y.set(0, 2);
+                } else if (code3_x.get(0) == 1 && code3_y.get(0) == 0 && code3_x.get(1) == code3_y.get(1) && code3_x.get(2) == code3_y.get(2)) {
+                    code3_x.set(0, 0);
+                    code3_y.set(0, 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < code3_x.size(); i++) {
+            code3.append("").append(code3_x.get(i)).append(code3_y.get(i));
+            code9.append(Integer.parseInt(code3.toString(), 3));
+            h_code.append(code9);
+            code3.setLength(0);
+            code9.setLength(0);
+        }
+
+        String h_2 = h_code.substring(3);
+        int h_1 = Integer.parseInt(h_code.substring(0, 3));
+        int h_a1 = (int) Math.floor(h_1 / 30);
+        int h_a2 = h_1 % 30;
+        StringBuffer h_code_r = new StringBuffer();
+        h_code_r.append(h_key.charAt(h_a1)).append(h_key.charAt(h_a2)).append(h_2.toString());
+        return new Zone(z_loc_y, z_loc_x, h_x, h_y, h_code_r.toString());
+    }
+
+    public static final XY adjustXY(double x, double y, int level) {
+        double max_hsteps = Math.pow(3, level + 2);
+        double hsteps = Math.abs(x - y);
+
+        if (hsteps == max_hsteps && x > y) {
+            double tmp = x;
+            x = y;
+            y = tmp;
+        } else if (hsteps > max_hsteps) {
+            double diff = hsteps - max_hsteps;
+            double diff_x = Math.floor(diff / 2);
+            double diff_y = diff - diff_x;
+            double edge_x;
+            double edge_y;
+
+            if (x > y) {
+                edge_x = x - diff_x;
+                edge_y = y + diff_y;
+                double h_xy = edge_x;
+                edge_x = edge_y;
+                edge_y = h_xy;
+                x = edge_x + diff_x;
+                y = edge_y - diff_y;
+            } else if (y > x) {
+                edge_x = x + diff_x;
+                edge_y = y - diff_y;
+                double h_xy = edge_x;
+                edge_x = edge_y;
+                edge_y = h_xy;
+                x = edge_x - diff_x;
+                y = edge_y + diff_y;
+            }
+        }
+
+        return new XY(x, y);
     }
 
     public static final class XY {
@@ -256,7 +316,15 @@ public class GeoHex {
         }
     }
 
-    // private static
+    public static final String encode(double lat, double lon, int level) {
+        return getZoneByLocation(lat, lon, level).code;
+    }
+
+    public static final Zone decode(String code) {
+        return getZoneByCode(code);
+    }
+
+
     private static XY loc2xy(double lon, double lat) {
         double x = lon * h_base / 180.0;
         double y = Math.log(Math.tan((90.0 + lat) * Math.PI / 360.0)) / (Math.PI / 180.0);
@@ -264,20 +332,11 @@ public class GeoHex {
         return new XY(x, y);
     }
 
-    // private static
     private static Loc xy2loc(double x, double y) {
         double lon = (x / h_base) * 180.0;
         double lat = (y / h_base) * 180.0;
         lat = 180 / Math.PI * (2.0 * Math.atan(Math.exp(lat * Math.PI / 180.0)) - Math.PI / 2.0);
         return new Loc(lat, lon);
-    }
-
-    public static final String encode(double lat, double lon, int level) {
-        return getZoneByLocation(lat, lon, level).code;
-    }
-
-    public static final Zone decode(String code) {
-        return getZoneByCode(code);
     }
 
     private static final Pattern INC15 = Pattern.compile("[15]");
