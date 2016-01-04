b8741f3e233a7e6fd626ca80dc12b0902265d69f
Sergey Shishkin <sergei.shishkin@gmail.com>
2016-01-04 19:01:00 +0800

    refactor JS code for in-browser interactive coding

--- js/src/main/javascript/main.js
+++ js/src/main/javascript/main.js
@@ -1,3 +1,25 @@
+function findRightLevel(map) {
+    var bb = map.getBounds();
+    var totalLon = Math.abs(bb.getNorth() - bb.getSouth());
+    var level = 1;
+    while (terahex.size(level) * 5 > totalLon) { level++; }
+    return level;
+}
+
+function showZone(map, loc, level) {
+    var zone = terahex.zoneByLocation(loc.lng, loc.lat, level);
+    var hex = omnivore.wkt
+        .parse(zone.wellKnownText)
+        .addTo(map);
+    var text = 'Code: ' + zone.code;
+    text += '<br />Level: ' + zone.level;
+    text += '<br />Location: ' + zone.location.lon + ',' + zone.location.lat;
+    L.popup()
+        .setLatLng(zone.location)
+        .setContent(text)
+        .openOn(map);
+}
+
 window.onload = function() {
 
     var map = L.map('map');
@@ -8,26 +30,10 @@ window.onload = function() {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    function findRightLevel() {
-        var bb = map.getBounds();
-        var totalLon = Math.abs(bb.getNorth() - bb.getSouth());
-        var level = 1;
-        while (terahex.size(level) * 5 > totalLon) { level++; }
-        return level;
-    }
 
     function onMapClick(e) {
-        var loc = e.latlng;
-        var level = findRightLevel();
-        var zone = terahex.zoneByLocation(loc.lng, loc.lat, level);
-        var hex = omnivore.wkt.parse(zone.wellKnownText).addTo(map);
-        var text = 'Code: ' + zone.code;
-        text += '<br />Level: ' + zone.level;
-        text += '<br />Location: ' + zone.location.lon + ',' + zone.location.lat;
-        L.popup()
-            .setLatLng(zone.location)
-            .setContent(text)
-            .openOn(map);
+        var level = findRightLevel(map);
+        showZone(map, e.latlng, level);
     }
 
     map.on('click', onMapClick);
